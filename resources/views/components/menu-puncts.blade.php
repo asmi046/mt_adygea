4c99deb2b0cf51f9d22a221b0a3d7744fae9af5f
asmi046 <asmi046@gmail.com>
2025-05-12 10:58:35 +0300

    Пункт меню поправил

--- resources/views/components/menu-puncts.blade.php
+++ resources/views/components/menu-puncts.blade.php
@@ -1,6 +1,6 @@
 <ul class="menue_puncts_list">
     <li><a class="yakor" href="#about">О туре</a></li>
-    <li><a class="yakor" href="#location">Лучшие моменты представления</a></li>
+    <li><a class="yakor" href="#location">Лучшие локации</a></li>
     <li><a class="yakor" href="#program">Программа тура</a></li>
     <li><a class="yakor" href="#in_tur">Купить тур</a></li>
     <li><a class="yakor" href="#contacts">Контакты</a></li>
